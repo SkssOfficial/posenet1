2e6cf055f80ffb93a06c13e1814936e80e6b92e8
C&MASKSS <78840918+SkssOfficial@users.noreply.github.com>
2021-04-20 11:54:13 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ let poses = [];
 let stage = 0;
 let stageResult = [0, 0]
 let poseOffset = 30
-let confidenceOffset = 0.6
+let confidenceOffset = 0.8
 let stopCounting = false
 
 let currentFrame = 0
@@ -30,7 +30,7 @@ let radio = 640 / 480
 let waitTime = 5;
 let waitTimeFuncVar;
 
-let nextStageTime = 70;
+let nextStageTime = 80;
 let nextStageTimeVar;
 
 // function preload() {
